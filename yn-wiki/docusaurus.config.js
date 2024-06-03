d936980626d4ea57cc57f93f36a5a53a2f6de018
yuning <sunyuning@ceair.com>
2024-06-03 10:40:43 +0800

    第一次设置GitHub用户名和项目名称

--- yn-wiki/docusaurus.config.js
+++ yn-wiki/docusaurus.config.js
@@ -6,13 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://yn-wiki.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Sunyunn', // Usually your GitHub org/user name.
+  projectName: 'myWiki', // Usually your repo name.
 
   presets: [
     [
